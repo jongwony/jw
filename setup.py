daf5ec65f727696bf67cb41cb5d89d259e6becd4
jongwony <lastone9182@gmail.com>
2019-04-28 03:58:17 +0900

    0.4 release: sqlalchemy, pandas is bio dependent

--- setup.py
+++ setup.py
@@ -4,20 +4,19 @@ from setuptools import setup, find_packages
 class Packages:
     crawl = ['requests', 'bs4']
     iterm = ['pygments', 'pyfiglet']
-    sql = ['sqlalchemy', 'pandas']
-    all = crawl + iterm + sql
+    all = crawl + iterm
 
 
 setup(
     name='jw',
-    version='0.3',
+    version='0.4',
     description='I just want to search and write my notes'
                 'regardless of their file name ...',
     author='jongwony',
     author_email='lastone9182@gmail.com',
     packages=find_packages(exclude=['tests*']),
     scripts=['jw'],
-    install_requires=[],
+    install_requires=['sqlalchemy', 'pandas'],
     extras_require={k: v for k, v in Packages.__dict__.items()
                     if not k.startswith('__')},
     package_data={'bio': ['config.ini', 'templates/*', 'scripts/*']},
