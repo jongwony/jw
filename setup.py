75170929c5ebcc557ee35d2b78887f451d83a799
jongwony <lastone9182@gmail.com>
2019-04-28 04:13:13 +0900

    pillow require install

--- setup.py
+++ setup.py
@@ -3,20 +3,18 @@ from setuptools import setup, find_packages
 
 class Packages:
     crawl = ['requests', 'bs4']
-    iterm = ['pygments', 'pyfiglet']
-    all = crawl + iterm
 
 
 setup(
     name='jw',
-    version='0.4',
+    version='0.5',
     description='I just want to search and write my notes'
                 'regardless of their file name ...',
     author='jongwony',
     author_email='lastone9182@gmail.com',
     packages=find_packages(exclude=['tests*']),
     scripts=['jw'],
-    install_requires=['sqlalchemy', 'pandas'],
+    install_requires=['sqlalchemy', 'pandas', 'Pillow', 'pygments', 'pyfiglet'],
     extras_require={k: v for k, v in Packages.__dict__.items()
                     if not k.startswith('__')},
     package_data={'bio': ['config.ini', 'templates/*', 'scripts/*']},
